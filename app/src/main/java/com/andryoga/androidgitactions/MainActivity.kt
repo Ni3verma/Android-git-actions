4ede149da9cadffbb3d3d2537787722ddafc5486
Nitin Verma <Nitin.Verma@softwareag.com>
2021-06-06 00:04:51 +0530

    'a'

--- app/src/main/java/com/andryoga/androidgitactions/MainActivity.kt
+++ app/src/main/java/com/andryoga/androidgitactions/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.andryoga.androidgitactions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var b=20
-        val a=10
     }
 }
